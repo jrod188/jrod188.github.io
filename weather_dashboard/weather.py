b5a69307af43df9a5d3fb16a87e96570ecfd6cca
Blue Owl <jrod188@gmail.com>
2019-01-01 21:23:05 -0800

    initialize

---
+++ weather_dashboard/weather.py
@@ -0,0 +1,229 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  1 20:17:27 2019
+
+@author: riceowl1
+"""
+
+#Dependencies
+from citipy import citipy as cp
+#import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+import time
+#import json
+import seaborn as sns
+from datetime import datetime
+#from pprint import pprint
+
+# Import API key
+api_key = "344569d22aedd5160e6055b93c2bd40a"
+
+# This n determines how many cities we generate
+# We also initialize our lists to track the cities
+
+# This n determines how many cities we generate
+# We also initialize our lists to track the cities
+
+# I chose 700 because many cities will not have data available
+# n=700 seemed safe in order to get above the 500 city threshold
+n = 750
+cities = []
+country_codes = []
+
+for x in range(n):
+    lat = np.random.uniform(-90, 90)
+    lng = np.random.uniform(-180, 180)
+    city = cp.nearest_city(lat, lng)
+    
+    # Make sure that the randomly generated city is not already in our series
+    # If it is, regenerate the lat and lng until we get one that isn't
+    while city.city_name in cities:
+        lat = np.random.uniform(-90, 90)
+        lng = np.random.uniform(-180, 180)
+        city = cp.nearest_city(lat, lng)
+    
+    # Add the data to our lists
+    cities.append(city.city_name)
+    country_codes.append(city.country_code)
+    
+country_codes = [x.upper() for x in country_codes]
+
+# Create base URL and indicate imperial units
+url = "http://api.openweathermap.org/data/2.5/weather?"
+units = "imperial"
+
+# Build partial query URL
+query = f"{url}appid={api_key}&units={units}&q="
+        
+# Create an "extracts" object to get the temperature, humidity, cloudiness and wind speed
+latitude = []
+longitude = []
+temperature = []
+humidity = []
+cloudiness = []
+wind_speed = []
+dates = []
+
+error = 0
+
+for x in range(len(cities)):
+    response = requests.get(f"{query}{cities[x]},{country_codes[x].upper()}").json()
+    time.sleep(1)
+
+    
+    # Some of the cities we generate don't have data in openweathermap, so set their values to numpy's NaN
+    try:
+        temperature.append(response['main']['temp_max'])
+        latitude.append(response['coord']['lat'])
+        longitude.append(response['coord']['lon'])
+        humidity.append(response['main']['humidity'])
+        wind_speed.append(response['wind']['speed'])
+        cloudiness.append(response['clouds']['all'])
+        dates.append(response['dt'])
+        print(f"Processing record {x + 1} | {cities[x]}")
+        print(f"{query}{cities[x]},{country_codes[x].upper()}")
+    except KeyError:
+        temperature.append(np.nan)
+        latitude.append(np.nan)
+        longitude.append(np.nan)
+        humidity.append(np.nan)
+        wind_speed.append(np.nan)
+        cloudiness.append(0)
+        dates.append(np.nan)
+        print(f"Error with record {x + 1} | {cities[x]} will be skipped")
+        error += 1
+    
+    # Sometimes it's not cloudy! Then 'clouds' does not exist, so set it to zero.
+#    try:
+#        cloudiness.append(response['clouds']['all'])
+#    except KeyError:
+#        cloudiness.append(0)
+#        print(f"Error with record {x + 1} | skipping {cities[x]}")
+    # Print each city name and query string    
+
+
+print("--------------------------------------------")
+print("Data Processing Complete")
+print("--------------------------------------------")
+
+# Assemble everything into a data frame
+weather_df = pd.DataFrame({"City": cities,
+                           "Latitude": latitude,
+                           "Longitude": longitude,
+                           "Humidity": humidity,
+                           "Max Temp": temperature,
+                           "Cloudiness": cloudiness,
+                           "Wind Speed": wind_speed,
+                           "Date": dates,
+                           "Country": country_codes
+                          })
+
+# Remove any cities that have NaN values
+weather_df.dropna(how='any', inplace = True)
+weather_df.drop_duplicates(inplace = True)
+
+print(f"The data frame contains {len(weather_df['City'])} unique cities and {str(error)} cities were dropped.")
+weather_df.head()
+# Set the aesthetic using Seaborn
+sns.set()
+
+# Increase the size of the figure
+plt.figure(figsize=(9,6))
+
+# Plot the lat vs the max temp
+plt.scatter(x = weather_df['Latitude'], y = weather_df['Max Temp'],
+            linewidths = 0.75, edgecolors = 'black', color = 'blue', s=35)
+
+# Add labeling
+# This extracts the date and returns it in the local time zone
+weather_dt = datetime.fromtimestamp(weather_df.loc[0,"Date"]).strftime('%Y-%m-%d')
+plt.title(f"Latitude vs. Max Temperature ({weather_dt})")
+plt.xlabel("Latitude")
+plt.ylabel("Max Temperature (F)")
+
+# Modify ticks
+plt.xlim(-80,100)
+plt.ylim(min(weather_df['Max Temp']) - 10, max(weather_df['Max Temp']) + 10)
+
+
+plt.show()
+plt.savefig("Latitude_Max_Temperature.png")
+
+# Set the aesthetic using Seaborn
+sns.set()
+
+# Increase the size of the figure
+plt.figure(figsize=(9,6))
+
+# Plot the lat vs the max temp
+plt.scatter(x = weather_df['Latitude'], y = weather_df['Humidity'],
+            linewidths = 0.75, edgecolors = 'black', color = 'blue', s=35)
+
+# Add labeling
+# This extracts the date and returns it in the local time zone
+weather_dt = datetime.fromtimestamp(weather_df.loc[0,"Date"]).strftime('%Y-%m-%d')
+plt.title(f"Latitude vs. Humidity ({weather_dt})")
+plt.xlabel("Latitude")
+plt.ylabel("Humidity (%)")
+
+# Modify ticks
+plt.xlim(-80,100)
+plt.ylim(-10, 110)
+
+
+plt.show()
+plt.savefig("Latitude_Humidity.png")
+# Set the aesthetic using Seaborn
+sns.set()
+
+# Increase the size of the figure
+plt.figure(figsize=(9,6))
+
+# Plot the lat vs the max temp
+plt.scatter(x = weather_df['Latitude'], y = weather_df['Cloudiness'],
+            linewidths = 0.75, edgecolors = 'black', color = 'blue', s=35)
+
+# Add labeling
+# This extracts the date and returns it in the local time zone
+weather_dt = datetime.fromtimestamp(weather_df.loc[0,"Date"]).strftime('%Y-%m-%d')
+plt.title(f"Latitude vs. Cloudiness ({weather_dt})")
+plt.xlabel("Latitude")
+plt.ylabel("Cloudiness (%)")
+
+# Modify ticks
+plt.xlim(-80,100)
+plt.ylim(-10, 110)
+
+
+plt.show()
+plt.savefig("Latitude_Cloudiness.png")
+
+
+# Set the aesthetic using Seaborn
+sns.set()
+
+# Increase the size of the figure
+plt.figure(figsize=(9,6))
+
+# Plot the lat vs the max temp
+plt.scatter(x = weather_df['Latitude'], y = weather_df['Wind Speed'],
+            linewidths = 0.75, edgecolors = 'black', color = 'blue', s=35)
+
+# Add labeling
+# This extracts the date and returns it in the local time zone
+weather_dt = datetime.fromtimestamp(weather_df.loc[0,"Date"]).strftime('%Y-%m-%d')
+plt.title(f"Latitude vs. Wind Speed ({weather_dt})")
+plt.xlabel("Latitude")
+plt.ylabel("Wind Speed (mph)")
+
+# Modify ticks
+plt.xlim(-80,100)
+plt.ylim(-5, max(weather_df['Wind Speed'])+5)
+
+
+plt.show()
+plt.savefig("Latitude_Windspeed.png")
